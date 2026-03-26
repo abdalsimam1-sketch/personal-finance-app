5394d0935229e196cc80c1e97bf7cb5c2da9aefe
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-26 20:53:19 +0100

    add : folder structure skeleton and basic routing skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
-function App() {}
+import { Budgets } from "./pages/Budgets";
+import { Overview } from "./pages/Overview";
+import { Pots } from "./pages/Pots";
+import { RecurringBills } from "./pages/RecurringBills";
+import { Transactions } from "./pages/Transactions";
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>}>
+          <Route index element={<Overview></Overview>}></Route>
+          <Route path="/budgets" element={<Budgets></Budgets>}></Route>
+          <Route path="/pots" element={<Pots></Pots>}></Route>
+          <Route
+            path="/bills"
+            element={<RecurringBills></RecurringBills>}
+          ></Route>
+          <Route
+            path="/transactions"
+            element={<Transactions></Transactions>}
+          ></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
 export default App;

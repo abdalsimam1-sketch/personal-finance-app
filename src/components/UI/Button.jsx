6876a9b007b5ddf2fefbcd284c3dea2c0fda6070
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-27 15:19:06 +0100

    add : btn-1 styles with hover states

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,6 +1,6 @@
 export const Button = ({ children, variant, onClick }) => {
   return (
-    <button className={` btn ${variant}  `} onClick={onClick}>
+    <button className={` btn btn-${variant}  `} onClick={onClick}>
       {children}
     </button>
   );

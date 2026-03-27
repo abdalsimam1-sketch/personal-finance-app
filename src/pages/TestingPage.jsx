7158f1698bd022ea4fbb23e333396e6a09f3fac3
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-27 15:34:52 +0100

    add : secondary buttons with its hover states

--- src/pages/TestingPage.jsx
+++ src/pages/TestingPage.jsx
@@ -5,7 +5,7 @@ export const TestingPage = () => {
       className="d-flex justify-content-center align-items-center"
       style={{ width: "100%", height: "100vh" }}
     >
-      <Button variant="primary" children="Placeholder"></Button>
+      <Button variant="secondary" children="Placeholder"></Button>
     </div>
   );
 };

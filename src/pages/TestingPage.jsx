8558c34c35140e58a8d0e9ca0f1d2e3263da1d67
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-27 15:41:13 +0100

    add : destroy button with its hover states

--- src/pages/TestingPage.jsx
+++ src/pages/TestingPage.jsx
@@ -5,7 +5,7 @@ export const TestingPage = () => {
       className="d-flex justify-content-center align-items-center"
       style={{ width: "100%", height: "100vh" }}
     >
-      <Button variant="secondary" children="Placeholder"></Button>
+      <Button variant="destroy" children="Placeholder"></Button>
     </div>
   );
 };

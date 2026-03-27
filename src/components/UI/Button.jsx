34ab51da33b85d7661116383db102f39668c3fae
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-27 16:01:26 +0100

    add : tertiary button and its hover states

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,7 +1,10 @@
 export const Button = ({ children, variant, onClick }) => {
   return (
-    <button className={` btn btn-${variant}  `} onClick={onClick}>
+    <button className={` btn btn-${variant} `} onClick={onClick}>
       {children}
+      {variant === "tertiary" && (
+        <i className="bi bi-caret-right-fill ms-2"></i>
+      )}
     </button>
   );
 };

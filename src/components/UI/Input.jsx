f39d1cbb29c6037c09df272076a6ca419a14f31f
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-29 22:42:02 +0100

    add : prefix input variant

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,15 +1,18 @@
-export const Input = ({ placeholder, helper, name, label }) => {
+export const Input = ({ placeholder, helper, name, label, variant }) => {
   return (
     <div>
       <label htmlFor={name} className="form-label fw-bold input-label">
         {label}
       </label>
-      <input
-        type="text"
-        placeholder={placeholder}
-        id={name}
-        className="form-control"
-      />
+      <div className="input-wrapper">
+        <span className="prefix text-muted">$</span>
+        <input
+          type="text"
+          placeholder={placeholder}
+          id={name}
+          className="form-control ps-4"
+        />
+      </div>
       <span className="align-self-end fw-bold text-end d-block helper-text">
         {helper}
       </span>

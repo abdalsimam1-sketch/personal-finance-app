e84c09e574f47199f3cec5568b35c507b9456279
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-03-30 20:04:09 +0100

    add : balance card component

---
+++ src/components/Layout/BalanceCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const BalanceCard = () => {
+  return (
+    <div className="card">
+      <span>Current Balance</span>
+      <h1>
+        <span>$</span>
+        4,836.00
+      </h1>
+    </div>
+  );
+};

2a4e2fbbfcb0edcc43972ad841fbd5aaf08c9e98
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-01 22:56:51 +0100

    fix : change date to BIllDate

--- src/HelperFunctions/CurrentDate.js
+++ src/HelperFunctions/CurrentDate.js
@@ -1,14 +1,14 @@
-const Today = new Date("2024-8-19");
+const Today = new Date("2024-8-1");
 const soon = new Date(Today.getTime() + 2 * 24 * 60 * 60 * 1000);
 export const CheckIfPaid = (date) => {
   const BillDate = new Date(date);
-  if (date > Today && date <= soon) {
+  if (BillDate > Today && BillDate <= soon) {
     return "soon";
-  } else if (date === Today) {
+  } else if (BillDate === Today) {
     return "due";
-  } else if (date > Today) {
+  } else if (BillDate > Today) {
     return "upcoming";
-  } else if (date < Today) {
+  } else if (BillDate < Today) {
     return "paid";
   }
 };

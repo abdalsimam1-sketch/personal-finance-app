98976e253a838684e85b64a9633f6a880d78f424
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-01 22:49:00 +0100

    fix : add more padding ,remove fixed width and rename amount to total

--- src/components/UI/BillsSummary.jsx
+++ src/components/UI/BillsSummary.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
-export const BillsSummary = ({ label, amount, color }) => {
+export const BillsSummary = ({ label, total, color }) => {
   return (
     <div
-      className="card p-2"
+      className="card p-3"
       style={{
-        width: "10rem",
         backgroundColor: "var(--color-beige-100)",
         borderLeft: `4px solid ${color}`,
       }}
     >
       <div className="d-flex justify-content-between">
         <span className="text-preset-4 text-muted">{label}</span>
-        <span className="fw-bold text-preset-4">{amount}</span>
+        <span className="fw-bold text-preset-4">${total}</span>
       </div>
     </div>
   );

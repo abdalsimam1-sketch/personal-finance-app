ab189745e7cff750a4f89977c9b1b67ba9eb6258
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-02 18:21:05 +0100

    add : filter section base layout before logic

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Title } from "../components/UI/Title";
+import { Input } from "../components/UI/Input";
 export const Transactions = () => {
   return (
     <div className="px-3 d-flex flex-column gap-3 ">
@@ -7,7 +8,25 @@ export const Transactions = () => {
         <Title>Transactions</Title>
       </section>
       <section className="col-12 card p-4">
-        <div className="filter-section"></div>
+        <div className="filter-section d-flex justify-content-between">
+          <Input placeholder="Search transaction" variant="icon"></Input>
+          <div className="d-flex gap-5">
+            <div>
+              <span className="text-muted text-preset-5">Sort by</span>
+              <div className="form-control  d-flex justify-content-between ">
+                <span className="text-preset-4"> Latest</span>
+                <i className="bi bi-caret-down-fill"></i>
+              </div>
+            </div>
+            <div>
+              <span className="text-muted text-preset-5">Category</span>
+              <div className="form-control d-flex justify-content-between">
+                <span className="text-preset-4"> All Transaction</span>
+                <i className="bi bi-caret-down-fill"></i>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="table-title"></div>
         <div className="transaction-list"></div>
         <div className="pagination-section"></div>

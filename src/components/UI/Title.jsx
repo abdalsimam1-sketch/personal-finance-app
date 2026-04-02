894e9e7921a6b6b1302dd41c4a653f78034fbfc6
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-02 14:51:01 +0100

    fix : remove margins from the title component

--- src/components/UI/Title.jsx
+++ src/components/UI/Title.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Title = ({ children }) => {
   return (
-    <div className="mt-5">
-      <h1 className="text-preset-1 ">{children}</h1>
+    <div>
+      <h1 className="text-preset-1 m-0">{children}</h1>
     </div>
   );
 };

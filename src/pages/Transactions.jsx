5fe55e928f632f97b51a0746f9958d8fc0e9e104
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-02 20:09:30 +0100

    add : dropdown menu structure and functions to opena dn close them

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Title } from "../components/UI/Title";
 import { Input } from "../components/UI/Input";
 import data from "../data/data.json";
 export const Transactions = () => {
+  const [SortIsOpen, SetSortIsOpen] = useState(false);
+  const [CategoryIsOpen, SetCategoryIsOpen] = useState(false);
+
+  const toggleSort = () => {
+    SetSortIsOpen(!SortIsOpen);
+  };
+  const toggleCategory = () => {
+    SetCategoryIsOpen(!CategoryIsOpen);
+  };
+
   return (
     <div className="px-3 d-flex flex-column gap-3 ">
       <section>
         <Title>Transactions</Title>
       </section>
       <section className="col-12 card p-4">
-        <div className="filter-section d-flex justify-content-between align-items-center">
+        <div className="filter-section d-flex justify-content-between align-items-center ">
           <div>
             <Input placeholder="Search transaction" variant="icon"></Input>
           </div>
           <div className="d-flex gap-5 ">
-            <div>
+            <div className="position-relative">
               <div className="d-none d-md-inline">
                 <span className="text-muted text-preset-5">Sort by</span>
                 <div className="form-control  d-flex justify-content-between  align-items-center gap-3">
@@ -23,17 +33,19 @@ export const Transactions = () => {
                 </div>
               </div>
               <i className=" btn bi bi-card-list m-0 d-md-none fs-2 "></i>
+              <div className="sort-dropdown position-absolute mt-3 top-100 start-0"></div>
             </div>
 
-            <div>
+            <div className="position-relative">
               <div className="d-none d-md-inline">
                 <span className="text-muted text-preset-5">Category</span>
                 <div className="form-control d-flex justify-content-between align-items-center gap-3">
-                  <span className="text-preset-4"> All Transaction</span>
+                  <span className="text-preset-4"> All Transactions</span>
                   <i className="bi bi-caret-down-fill"></i>
                 </div>
               </div>
               <i className="btn bi bi-funnel-fill m-0 d-md-none fs-2"></i>
+              <div className="category-dropdown position-absolute mt-3 top-100 end-0"></div>
             </div>
           </div>
         </div>

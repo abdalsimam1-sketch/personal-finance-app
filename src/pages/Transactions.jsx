f7ad1d27f0cf0b21d802aaeb317f4b7af817a3d5
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-02 18:42:48 +0100

    add : icons for the mobile and make them buttons

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -8,22 +8,31 @@ export const Transactions = () => {
         <Title>Transactions</Title>
       </section>
       <section className="col-12 card p-4">
-        <div className="filter-section d-flex justify-content-between">
-          <Input placeholder="Search transaction" variant="icon"></Input>
-          <div className="d-flex gap-5">
+        <div className="filter-section d-flex justify-content-between align-items-center">
+          <div>
+            <Input placeholder="Search transaction" variant="icon"></Input>
+          </div>
+          <div className="d-flex gap-5 ">
             <div>
-              <span className="text-muted text-preset-5">Sort by</span>
-              <div className="form-control  d-flex justify-content-between ">
-                <span className="text-preset-4"> Latest</span>
-                <i className="bi bi-caret-down-fill"></i>
+              <div className="d-none d-md-inline">
+                <span className="text-muted text-preset-5">Sort by</span>
+                <div className="form-control  d-flex justify-content-between  align-items-center gap-3">
+                  <span className="text-preset-4"> Latest</span>
+                  <i className="bi bi-caret-down-fill"></i>
+                </div>
               </div>
+              <i className=" btn bi bi-card-list m-0 d-md-none fs-2 "></i>
             </div>
+
             <div>
-              <span className="text-muted text-preset-5">Category</span>
-              <div className="form-control d-flex justify-content-between">
-                <span className="text-preset-4"> All Transaction</span>
-                <i className="bi bi-caret-down-fill"></i>
+              <div className="d-none d-md-inline">
+                <span className="text-muted text-preset-5">Category</span>
+                <div className="form-control d-flex justify-content-between align-items-center gap-3">
+                  <span className="text-preset-4"> All Transaction</span>
+                  <i className="bi bi-caret-down-fill"></i>
+                </div>
               </div>
+              <i className="btn bi bi-funnel-fill m-0 d-md-none fs-2"></i>
             </div>
           </div>
         </div>

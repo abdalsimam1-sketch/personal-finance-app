08dc5d4ea4d05aea70f311383d4ddd12e1e2dd74
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-05 22:14:34 +0100

    add : spacing between sections

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -2,12 +2,12 @@ import { Title } from "../components/UI/Title";
 import { BudgetCard } from "../components/UI/BudgetCard";
 export const Budgets = () => {
   return (
-    <div className="container">
+    <div className="container d-flex flex-column gap-3">
       <div className="d-flex justify-content-between align-items-center mt-4">
         <Title>Budgets</Title>
         <button className="btn text-light bg-dark">+ Add New Budget</button>
       </div>
-      <div className="row">
+      <div className="row ">
         <section className="col-12 col-md-5"></section>
         <section className="col-12 col-md-7 min-vh-100">
           <BudgetCard></BudgetCard>

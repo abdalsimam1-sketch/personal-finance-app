bcf69f253950ef09c36bcb697c2e26c90b854a2a
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-05 15:04:52 +0100

    add : title component

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -1,6 +1,9 @@
+import { Title } from "../components/UI/Title";
+
 export const Budgets = () => {
   return (
     <div className="container">
+      <Title>Budgets</Title>
       <div className="row">
         <section className="col-12 col-md-5"></section>
         <section className="col-12 col-md-7"></section>

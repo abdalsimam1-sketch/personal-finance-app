567bf8c43b3d40e25bd9e803ff4e18ab742a4c74
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-05 15:08:01 +0100

    add : add new budget button

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -3,7 +3,10 @@ import { Title } from "../components/UI/Title";
 export const Budgets = () => {
   return (
     <div className="container">
-      <Title>Budgets</Title>
+      <div className="d-flex justify-content-between align-items-center mt-4">
+        <Title>Budgets</Title>
+        <button className="btn text-light bg-dark">+ Add New Budget</button>
+      </div>
       <div className="row">
         <section className="col-12 col-md-5"></section>
         <section className="col-12 col-md-7"></section>

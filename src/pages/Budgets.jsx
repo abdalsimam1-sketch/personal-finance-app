f748f0fb79c75d46bde1c7c662e1cc75f521d07c
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-05 14:19:16 +0100

    add : base page structure

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-
 export const Budgets = () => {
-  return <div>Budgets</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <section className="col-12 col-md-5"></section>
+        <section className="col-12 col-md-7"></section>
+      </div>
+    </div>
+  );
 };

0909ca7be939b5952eb813771ea67773a90ee668
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-06 20:19:38 +0100

    add : title component and add pots button

--- src/pages/Pots.jsx
+++ src/pages/Pots.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import { Title } from "../components/UI/Title";
 export const Pots = () => {
-  return <div>Pots</div>;
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-between align-items-center mt-4">
+        <Title>Pots</Title>
+        <button className="btn bg-dark text-light">+ Add New Pot</button>
+      </div>
+    </div>
+  );
 };

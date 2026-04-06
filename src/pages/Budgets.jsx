64ddd6a34605d16b0d23653e5f86cd452cb50749
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-06 14:59:51 +0100

    add : pie chart

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -1,6 +1,7 @@
 import { Title } from "../components/UI/Title";
 import { BudgetCard } from "../components/UI/BudgetCard";
 import data from "../data/data.json";
+import { BudgetPieChart } from "../components/UI/BudgetPieChart";
 const budgets = data.budgets;
 
 export const Budgets = () => {
@@ -16,16 +17,32 @@ export const Budgets = () => {
       .filter((item) => item.category === category)
       .reduce((sum, item) => sum + item.amount, 0);
   };
+  const totalSpent = budgets.reduce(
+    (sum, item) => sum + Math.abs(totalSpentForEachCategory(item.category)),
+    0,
+  );
 
+  const PieChartData = budgets.map((item) => ({
+    name: item.category,
+    value: Math.abs(totalSpentForEachCategory(item.category)),
+    color: item.theme,
+  }));
+  const limit = budgets.reduce((sum, item) => sum + item.maximum, 0);
   return (
     <div className="container d-flex flex-column gap-3">
       <div className="d-flex justify-content-between align-items-center mt-4">
         <Title>Budgets</Title>
         <button className="btn text-light bg-dark">+ Add New Budget</button>
       </div>
-      <div className="row ">
-        <section className="col-12 col-md-5"></section>
-        <section className="col-12 col-md-7 min-vh-100">
+      <div className="row align-items-start">
+        <section className="col-12 col-md-5 card ">
+          <BudgetPieChart
+            data={PieChartData}
+            TotalSpent={totalSpent}
+            TotalLimit={limit}
+          ></BudgetPieChart>
+        </section>
+        <section className="col-12 col-md-7 min-vh-100 flex-grow-1">
           {budgets.map((item) => (
             <div className="d-flex flex-column mb-3">
               <BudgetCard

270a1fa1a906be946ad3bfc6aa25201b2702bd39
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-06 20:46:58 +0100

    add : base pots card for easir mapping

---
+++ src/components/UI/PotsCard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button } from "../UI/Button";
+import { BudgetsAndPotsLabel } from "../UI/BudgetsAndPotsLabel";
+export const PotsCard = ({ total, color, percentage, target, label }) => {
+  return (
+    <div className="card p-4 col-11 mx-auto">
+      <BudgetsAndPotsLabel
+        variant="Pot"
+        label={label}
+        color={color}
+      ></BudgetsAndPotsLabel>
+      <section className="d-flex justify-content-between align-items-center ">
+        <span className="text-preset-4 text-muted">Total Saved</span>
+        <h1 className="text-preset-1">${total}</h1>
+      </section>
+      <section
+        className="percentage-section card w-100"
+        style={{ height: "10px", backgroundColor: "var(--color-beige-100)" }}
+      >
+        <div
+          className="rounded"
+          style={{
+            height: "100%",
+            backgroundColor: "var(--color-green)",
+            width: `{${percentage}%`,
+          }}
+        ></div>
+      </section>
+      <section className="text-preset-5 text-muted d-flex justify-content-between align-items-center">
+        <span>{percentage}</span>
+        <span>Target of {target}</span>
+      </section>
+      <section className="d-flex justify-content-between">
+        <Button variant="secondary">+ Add Money</Button>
+        <Button variant="secondary">Withdraw</Button>
+      </section>
+    </div>
+  );
+};
--- src/pages/Pots.jsx
+++ src/pages/Pots.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Title } from "../components/UI/Title";
+import { PotsCard } from "../components/UI/PotsCard";
+import { Button } from "../components/UI/Button";
 export const Pots = () => {
   return (
-    <div className="container">
+    <div className="container d-flex flex-column gap-3 ">
       <div className="d-flex justify-content-between align-items-center mt-4">
         <Title>Pots</Title>
-        <button className="btn bg-dark text-light">+ Add New Pot</button>
+        <Button variant="primary">+ Add New Pot</Button>
       </div>
+      <section className="row">
+        <PotsCard label="Savings"></PotsCard>
+      </section>
     </div>
   );
 };

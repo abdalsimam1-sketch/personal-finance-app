b2a017ab6f24eb1321b7d0cceb0a2f3428b2de14
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-06 13:46:06 +0100

    add : the latest 3 spendings for each category

--- src/components/UI/BudgetCard.jsx
+++ src/components/UI/BudgetCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { BudgetsAndPotsLabel } from "./BudgetsAndPotsLabel";
 import { PercantageBar } from "./PercantageBar";
-export const BudgetCard = ({ maximum, category, theme, percentage }) => {
+import { FormatDate } from "../../HelperFunctions/DateFormat";
+import { SeeDetails } from "../../components/UI/SeeDetails";
+export const BudgetCard = ({
+  maximum,
+  category,
+  theme,
+  percentage,
+  latest3 = [],
+}) => {
+  const path = "/transactions";
   return (
     <div className="card p-4 d-flex flex-column gap-3">
       <BudgetsAndPotsLabel
@@ -26,6 +35,38 @@ export const BudgetCard = ({ maximum, category, theme, percentage }) => {
             <span className="fw-bold text-preset-4">$</span>
           </div>
         </div>
+      </div>
+      <div
+        className="card p-3"
+        style={{ background: "var(--color-beige-100)" }}
+      >
+        <SeeDetails
+          path={path}
+          SeeDetail="See More"
+          label="Latest Spending"
+        ></SeeDetails>
+        {latest3.map((item) => (
+          <div className=" d-flex justify-content-between mb-3">
+            <div className="d-flex align-items-center gap-3">
+              <img
+                src={item.avatar}
+                alt={item.name + " avatar"}
+                style={{ width: "3rem", borderRadius: "50%" }}
+              />
+              <span className="text-preset-4 fw-bold">{item.name}</span>
+            </div>
+            <div className="d-flex flex-column">
+              <span
+                className={`text-preset-4 fw-bold ${item.amount < 0 ? "text-danger" : " text-success"}`}
+              >
+                {item.amount < 0 && <span>-</span>} ${Math.abs(item.amount)}
+              </span>
+              <span className="text-preset-4 text-muted">
+                {FormatDate(item.date)}
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -4,6 +4,12 @@ import data from "../data/data.json";
 const budgets = data.budgets;
 
 export const Budgets = () => {
+  const getLatest3 = (category) => {
+    return data.transactions
+      .filter((item) => item.category === category)
+      .sort((a, b) => new Date(b.date) - new Date(a.date))
+      .slice(0, 3);
+  };
   return (
     <div className="container d-flex flex-column gap-3">
       <div className="d-flex justify-content-between align-items-center mt-4">
@@ -20,6 +26,7 @@ export const Budgets = () => {
                 theme={item.theme}
                 category={item.category}
                 percentage={item.maximum / 100}
+                latest3={getLatest3(item.category)}
               ></BudgetCard>
             </div>
           ))}

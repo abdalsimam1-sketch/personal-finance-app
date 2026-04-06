330676cb7b2919a576a715ccdb20f6e6b229720c
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-06 22:11:51 +0100

    add : page title conponent

--- src/pages/RecurringBills.jsx
+++ src/pages/RecurringBills.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import { Title } from "../components/UI/Title";
 export const RecurringBills = () => {
-  return <div>RecurringBills</div>;
+  return (
+    <div className="container">
+      <div className="mt-4">
+        <Title>Recurring Bills</Title>
+      </div>
+    </div>
+  );
 };

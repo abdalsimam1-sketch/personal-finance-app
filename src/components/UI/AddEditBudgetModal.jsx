994d7c83c79dae5e1cff12d126848ad909ff63c8
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-09 08:58:34 +0100

    refactor : make themes reusable

--- src/components/UI/AddEditBudgetModal.jsx
+++ src/components/UI/AddEditBudgetModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Input } from "./Input";
 import { Button } from "../UI/Button";
+import { themes } from "../../data/Themes";
 export const AddEditBudgetModal = ({
   theme,
   onClose,
@@ -9,36 +10,6 @@ export const AddEditBudgetModal = ({
   maximum,
   usedThemes,
 }) => {
-  const themes = [
-    {
-      name: "Green",
-      theme: "#277C78",
-    },
-    {
-      name: "Yellow",
-      theme: "#82C9D7",
-    },
-    {
-      name: "Cyan",
-      theme: "#F2CDAC",
-    },
-    {
-      name: "Navy",
-      theme: "#626070",
-    },
-    {
-      name: "Red",
-      theme: "#FF6B6B",
-    },
-    {
-      name: "Purple",
-      theme: "#9B5DE5",
-    },
-    {
-      name: "Turquoise",
-      theme: "#00C2BA",
-    },
-  ];
   const categories = [
     {
       name: "Entertainment",

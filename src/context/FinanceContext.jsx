eb1f35070e39c1fe448a80082214438a3d4f93f0
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-10 23:02:51 +0100

    add : values to be shared through out the app

--- src/context/FinanceContext.jsx
+++ src/context/FinanceContext.jsx
@@ -3,7 +3,36 @@ import { useState, useEffect, useContext, createContext } from "react";
 const FinanceContext = createContext();
 
 export const FinanceProvider = ({ children }) => {
-  const valuesToBeShared = {};
+  //state
+  const [balance, setBalance] = useState();
+  const [transactions, setTransactions] = useState();
+  const [budgets, setBudgets] = useState();
+  const [pots, setPots] = useState();
+
+  //actions
+  const addBudget = () => {};
+  const editBudget = () => {};
+  const deleteBudget = () => {};
+  const addPot = () => {};
+  const editPot = () => {};
+  const deletePot = () => {};
+  const addMoney = () => {};
+  const withdrawMoney = () => {};
+
+  const valuesToBeShared = {
+    balance,
+    transactions,
+    budgets,
+    pots,
+    addBudget,
+    editBudget,
+    deleteBudget,
+    addPot,
+    editPot,
+    deletePot,
+    addMoney,
+    withdrawMoney,
+  };
   return (
     <FinanceContext.Provider value={valuesToBeShared}>
       {children}

5d3ede48f434d209923be108a1355fb6c345d845
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-11 10:15:27 +0100

    add : import data and make a function to add budgets

--- src/context/FinanceContext.jsx
+++ src/context/FinanceContext.jsx
@@ -1,16 +1,32 @@
 import { useState, useEffect, useContext, createContext } from "react";
-
+import data from "../data/data.json";
 const FinanceContext = createContext();
 
 export const FinanceProvider = ({ children }) => {
   //state
-  const [balance, setBalance] = useState();
-  const [transactions, setTransactions] = useState();
-  const [budgets, setBudgets] = useState();
-  const [pots, setPots] = useState();
+  const [balance, setBalance] = useState(data.balance);
+  const [transactions, setTransactions] = useState(data.transactions);
+  const [budgets, setBudgets] = useState(data.budgets);
+  const [pots, setPots] = useState(data.pots);
 
   //actions
-  const addBudget = () => {};
+  const addBudget = ({ category, maximum, theme }) => {
+    const budgetExists = budgets.find((item) => {
+      return item.category === category;
+    });
+    if (budgetExists) {
+      return;
+    } else {
+      setBudgets((current) => [
+        {
+          category,
+          maximum: Number(maximum) || 0,
+          theme: theme.theme,
+        },
+        ...current,
+      ]);
+    }
+  };
   const editBudget = () => {};
   const deleteBudget = () => {};
   const addPot = () => {};

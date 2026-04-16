b20b61c1570dff22920af021ca4bef00e2ba0389
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-16 20:31:36 +0100

    add : wrap login in public routes and app in protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,33 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { LoginPage } from "./pages/LoginPage";
 import { Navigate } from "react-router-dom";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { PublicRoute } from "./components/PublicRoute";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-        <Route path="/" element={<Navigate to="/login"></Navigate>}></Route>
-        <Route path="/app" element={<Layout></Layout>}>
-          <Route index element={<Overview></Overview>}></Route>
-          <Route path="budgets" element={<Budgets></Budgets>}></Route>
-          <Route path="pots" element={<Pots></Pots>}></Route>
-          <Route
-            path="bills"
-            element={<RecurringBills></RecurringBills>}
-          ></Route>
-          <Route
-            path="transactions"
-            element={<Transactions></Transactions>}
-          ></Route>
+        {/*  Public Routes */}
+        <Route element={<PublicRoute></PublicRoute>}>
+          <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+          <Route path="/" element={<Navigate to="/login"></Navigate>}></Route>
+        </Route>
+
+        {/*  Protected Routes */}
+        <Route element={<ProtectedRoute></ProtectedRoute>}>
+          <Route path="/app" element={<Layout></Layout>}>
+            <Route index element={<Overview></Overview>}></Route>
+            <Route path="budgets" element={<Budgets></Budgets>}></Route>
+            <Route path="pots" element={<Pots></Pots>}></Route>
+            <Route
+              path="bills"
+              element={<RecurringBills></RecurringBills>}
+            ></Route>
+            <Route
+              path="transactions"
+              element={<Transactions></Transactions>}
+            ></Route>
+          </Route>
         </Route>
         <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
       </Routes>

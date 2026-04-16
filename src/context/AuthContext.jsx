df77aa4835be2271f951f9fe5d96ac46af9e49d3
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-04-16 14:30:57 +0100

    add : auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import { supabase } from "../HelperFunctions/supabaseClient";
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getInitialSession = async () => {
+      const result = await supabase.auth.getSession();
+      const session = result.data.session;
+      setSession(session);
+      setLoading(false);
+    };
+    getInitialSession();
+  }, []);
+  const valuesToBeShared = { session, user, loading };
+  return (
+    <AuthContext.Provider value={valuesToBeShared}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};

c6813a8edaf053a9a6878dc707caf22c8c80750a
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-09-11 19:22:07 +0100

    chore : add basic logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,12 +4,14 @@ import express,{type Express} from "express"
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import morgan from "morgan"
 import { notFound } from "./middleware/notFound.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 export const app : Express = express();
 
 app.use(express.json())
+app.use(morgan("dev"))
 app.use(helmet())
 app.use(cors({
     origin:process.env.CLIENT_URL,
@@ -19,7 +21,18 @@ app.use(rateLimit({
     windowMs:10*60*1000,
     max:100
 }))
-app.all("*",notFound)
+
+app.use("/health",(req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Health check passed",
+    data:{
+        
+    }
+  });
+    
+})
+app.all("/{*catchall}",notFound)
 app.use(errorHandler)
 
 

d630de8d5f2c2310baa22953abc3eb93cf234294
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-09-16 10:51:14 +0100

    feat : mount auth routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,38 +1,43 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express,{type Express} from "express"
+import "dotenv/config";
+import express, { type Express } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
-import morgan from "morgan"
+import morgan from "morgan";
 import { notFound } from "./middleware/notFound.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
-export const app : Express = express();
+//routes import
+import { authRouter } from "./routes/auth.routes.js";
 
-app.use(express.json())
-app.use(morgan("dev"))
-app.use(helmet())
-app.use(cors({
-    origin:process.env.CLIENT_URL,
-    credentials:true
-}))
-app.use(rateLimit({
-    windowMs:10*60*1000,
-    max:100
-}))
+export const app: Express = express();
 
-app.use("/health",(req, res) => {
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100,
+  }),
+);
+
+app.use("/health", (req, res) => {
   res.status(200).json({
     success: true,
     message: "Health check passed",
-    data:{
-        
-    }
+    data: {},
   });
-    
-})
-app.all("/{*catchall}",notFound)
-app.use(errorHandler)
+});
 
+//routes
+app.use("/api/v1/auth", authRouter);
 
+app.all("/{*catchall}", notFound);
+app.use(errorHandler);

aca3c1b4f77c5abc3c50cdf241ce2c4f754bb0ff
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-09-16 10:49:40 +0100

    refactor : change 500 error response shape

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,19 +1,25 @@
-import {  CustomError, InternalServerError } from "../errors/errors.js"
-import type {Request,Response ,NextFunction} from "express"
+import { CustomError, InternalServerError } from "../errors/errors.js";
+import type { Request, Response, NextFunction } from "express";
 
-
-export const errorHandler = (error:Error,req:Request,res:Response,next:NextFunction)=>{
-  if (error instanceof CustomError&& error.isOperational) {
-    return res
-      .status(error.statusCode)
-      .json({
-        success: false,
-        message: error.message,
-        statusCode: error.statusCode,
-      });
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (error instanceof CustomError && error.isOperational) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+      statusCode: error.statusCode,
+    });
   }
 
-  console.error("Error : ", error)
+  console.error("Error : ", error);
 
-  throw new InternalServerError();
-}
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+    statusCode: 500,
+  });
+};

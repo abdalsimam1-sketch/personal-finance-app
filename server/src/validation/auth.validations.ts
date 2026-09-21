7b6aee087e05ebb3300973803e0967e04f833bb0
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-09-21 18:30:33 +0100

    feat : reset password validation

--- server/src/validation/auth.validations.ts
+++ server/src/validation/auth.validations.ts
@@ -22,3 +22,19 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+export const resetPasswordSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(8)
+      .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+      .regex(/[0-9]/, "Must contain at least one number")
+      .regex(/[a-z]/, "Must contain at least one lowercase letter")
+      .regex(/[^A-Za-z0-9]/, "Must contain at least one special character"),
+    confirmNewPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    message: "Passwords do not match",
+    path: ["confirmNewPassword"],
+  });
